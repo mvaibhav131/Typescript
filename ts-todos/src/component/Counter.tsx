017b501905fbcac9f99866ae6f0f8392e1eb2b97
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-02-17 16:37:40 +0530

    setup and making the folder

---
+++ ts-todos/src/component/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div>Counter</div>
+  )
+}
+
+export default Counter
+
+
+//diffrence between the react fragment vs div  (i,e : <></> vs <div></div>)
